5e375095d339cfd4a44ca81b5dbd9f97c32ba350
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-21 14:09:57 +0700

    Initial Commit

---
+++ src/jobsheet8/Latihan2b.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet8;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan2b {
+    public static void main (String [] args){
+        int a = 1;
+          do
+          {
+          System.out.println(a);
+          a++;
+          }
+          while ( a < 0 );
+
+}
+}
---
+++ src/jobsheet8/Praktikum.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet8;
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class Praktikum {
+    
+
+    public static void main(String[] args) {
+
+        // membuat variabel dan scanner
+        boolean running = true;
+        int counter = 0;
+        String jawab;
+        Scanner scan = new Scanner(System.in);
+
+        while( running ) {
+            System.out.println("Apakah anda ingin keluar?");
+            System.out.print("Jawab [ya/tidak]> ");
+
+            jawab = scan.nextLine();
+            
+            if( jawab.equalsIgnoreCase("tidak") ){
+                running = true;
+            }
+            // cek jawabnnya, kalau ya maka berhenti mengulang
+            if( jawab.equalsIgnoreCase("ya") ){
+                running = false;
+            }
+
+            counter++;
+        }
+
+        System.out.println("Anda sudah melakukan perulangan sebanyak " + counter + " kali");
+
+    }
+
+}
